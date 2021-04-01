b2bd5b83fcda41356ca20f2cbd2a531e3915043b
Philippe Gairaud <philippegq@gmail.com>
2021-04-01 17:37:05 -0600

    login initialization

--- src/main/java/com/example/Airline_system/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.Airline_system;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
