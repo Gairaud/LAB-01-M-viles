9fec457d8a2159c2eae2fae88bda3539fa93af41
Philippe Gairaud <philippegq@gmail.com>
2021-04-01 21:23:38 -0600

    Create User.java

---
+++ src/main/java/com/ast/airlinesystem/model/User.java
@@ -0,0 +1,103 @@
+package com.ast.airlinesystem.model;
+
+public class User {
+
+    private int id;
+    private String userName;
+    private String password;
+    private String name;
+    private String lastName;
+    private String email;
+    private String addres;
+    private String phone;
+    private int isAdmin;
+
+    public User(int id, String userName, String password, String name, String lastName,
+                String email, String addres, String phone, int isAdmin) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.addres = addres;
+        this.phone = phone;
+        this.isAdmin = isAdmin;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(int isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+
+
+
+}
